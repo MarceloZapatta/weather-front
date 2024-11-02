084e4daae41fc810a24a080c77ac18c533c08ffb
Marcelo Zapatta <marcelozapatta0@gmail.com>
2024-11-02 13:48:18 -0300

    feat: persisting user in store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,9 +1,19 @@
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { defineStore } from 'pinia'
 import type { User } from '@/interfaces/user'
 
 export const useUserStore = defineStore('user', () => {
-  const user = ref<User | null>(null)
+  const user = ref<User | null>(
+    JSON.parse(localStorage.getItem('user') || 'null'),
+  )
+
+  watch(
+    user,
+    newUser => {
+      localStorage.setItem('user', JSON.stringify(newUser))
+    },
+    { deep: true },
+  )
 
   return { user }
 })
